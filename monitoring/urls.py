ca978a300462a4b649ca7b5039339b4a245ee66d
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2018-07-19 17:11:54 +0100

    Repoint urls to new availability app location

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^availability/', include('availability.urls')),
+    url(r'^availability/', include('monitoring.availability.urls')),
 ]
