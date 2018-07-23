27a641ffb30eea5d08ee30e8f76360169382ea67
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2018-07-23 16:12:51 +0100

    Add basic model serialiser and view for cloud

--- monitoring/publishing/views.py
+++ monitoring/publishing/views.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from rest_framework import viewsets
+from models import CloudSite
+from serializers import CloudSiteSerializer
+
+
+class CloudSiteViewSet(viewsets.ModelViewSet):
+    queryset = CloudSite.objects.all()
+    serializer_class = CloudSiteSerializer
--- monitoring/urls.py
+++ monitoring/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^availability/', include('monitoring.availability.urls')),
-    # url(r'^publishing/', include('monitoring.publishing.urls')),
+    url(r'^publishing/', include('monitoring.publishing.urls')),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
 ]
