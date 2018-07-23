b48ba232738e50504da6e2a4d87e41454f48777e
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2018-07-23 15:27:47 +0100

    Add simple status page that checks if time is even

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -1,4 +1,4 @@
-"""monitoring URL Configuration
+"""Monitoring URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -19,4 +19,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^availability/', include('monitoring.availability.urls')),
+    # url(r'^publishing/', include('monitoring.publishing.urls')),
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
 ]
