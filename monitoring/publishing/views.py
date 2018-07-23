435f8315963b1c2ff3fcd078cd08783728056ee1
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2018-07-23 18:10:10 +0100

    Switch to template view

--- monitoring/publishing/views.py
+++ monitoring/publishing/views.py
@@ -2,9 +2,6 @@
 from __future__ import unicode_literals
 
 
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
 from rest_framework import viewsets
 from models import CloudSite
 from serializers import CloudSiteSerializer
@@ -13,3 +10,10 @@ from serializers import CloudSiteSerializer
 class CloudSiteViewSet(viewsets.ModelViewSet):
     queryset = CloudSite.objects.all()
     serializer_class = CloudSiteSerializer
+    template_name = 'cloudsites.html'
+
+    def list(self, request):
+        ret = super(CloudSiteViewSet, self).list(request)
+        # Wrap data in dict so it can display in template.
+        ret.data = {'sites': ret.data}
+        return ret
