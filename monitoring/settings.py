c5ecd6979e0f00e8142d31b0e719ca298c645899
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2018-07-23 18:10:10 +0100

    Switch to template view

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -45,7 +45,7 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-        'rest_framework.renderers.StaticHTMLRenderer',
+        'rest_framework.renderers.TemplateHTMLRenderer',
     )
 }
 
