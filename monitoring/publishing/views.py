58c79ddb783a84c973fb4033e2235a28e9d1e18a
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2019-04-30 16:54:14 +0100

    Add basic single site retrieve view method

--- monitoring/publishing/views.py
+++ monitoring/publishing/views.py
@@ -32,3 +32,21 @@ class CloudSiteViewSet(viewsets.ReadOnlyModelViewSet):
         if type(request.accepted_renderer) is TemplateHTMLRenderer:
             response.data = {'sites': response.data, 'last_fetched': last_fetched}
         return response
+
+    def retrieve(self, request, pk=None):
+        last_fetched = CloudSite.objects.aggregate(Max('fetched'))['fetched__max']
+        print last_fetched.replace(tzinfo=None), datetime.today() - timedelta(hours=1, seconds=20)
+        if last_fetched.replace(tzinfo=None) < (datetime.today() - timedelta(hours=1, seconds=20)):
+            print 'Out of date'
+            fetchset =  VAnonCloudRecord.objects.using('cloud').raw("SELECT b.SiteName, COUNT(DISTINCT VMUUID), CloudType, b.UpdateTime FROM (SELECT SiteName, MAX(UpdateTime) AS latest FROM VAnonCloudRecords WHERE UpdateTime>'2018-07-25' GROUP BY SiteName) AS a INNER JOIN VAnonCloudRecords AS b ON b.SiteName = a.SiteName AND b.UpdateTime = a.latest GROUP BY SiteName")
+            for f in fetchset:
+                CloudSite.objects.update_or_create(defaults={'script': f.CloudType, 'updated': f.UpdateTime}, name=f.SiteName)
+        else:
+            print 'No need to update'
+
+        response = super(CloudSiteViewSet, self).retrieve(request)
+        # Wrap data in a dict so that it can display in template.
+        if type(request.accepted_renderer) is TemplateHTMLRenderer:
+            # Single result put in list to work with same HTML template.
+            response.data = {'sites': [response.data], 'last_fetched': last_fetched}
+        return response
