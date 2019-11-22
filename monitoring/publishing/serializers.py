18b838fa4284be8900e15efc99ea87da4e51f9e9
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2019-11-22 16:44:05 +0000

    Fix formatting of datetime in cloud HTML

--- monitoring/publishing/serializers.py
+++ monitoring/publishing/serializers.py
@@ -10,6 +10,11 @@ class GridSiteSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class CloudSiteSerializer(serializers.HyperlinkedModelSerializer):
+    # Override default format with None so that Python datetime is used as
+    # ouput format. Encoding will be determined by the renderer and can be
+    # formatted by a template filter.
+    updated = serializers.DateTimeField(format=None)
+
     class Meta:
         model = CloudSite
         fields = ('url', 'name', 'vms', 'script', 'updated')
