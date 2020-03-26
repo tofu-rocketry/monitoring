05908f399ba3a7c8bb3f7d3ebfd956a8fb6ec85e
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2020-03-26 16:24:02 +0000

    Add basic models

--- monitoring/sychronisation/models.py
+++ monitoring/sychronisation/models.py
@@ -3,4 +3,30 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class GridSite(models.Model):
+    fetched = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, primary_key=True)
+    updated = models.DateTimeField()
+
+
+class VSuperSummaries(models.Model):
+    Site = models.CharField(max_length=255, primary_key=True)
+    Year = models.IntegerField(primary_key=True)
+    Month = models.IntegerField(primary_key=True)
+    NumberOfJobs = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'VSuperSummaries'
+
+
+class VSyncRecords(models.Model):
+    Site = models.CharField(max_length=255, primary_key=True)
+    Year = models.IntegerField(primary_key=True)
+    Month = models.IntegerField(primary_key=True)
+    NumberOfJobs = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'VSyncRecords'
