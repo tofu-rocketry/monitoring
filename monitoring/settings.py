de73ce2af0db5463f7ffd738e40f6f055f59aed2
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2020-03-26 17:39:35 +0000

    Add sync to apps

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -93,6 +93,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'monitoring.publishing',
     'monitoring.availability',
+    'monitoring.synchronisation',
     'monitoring.benchmarks',
     'monitoring.iris',
     'monitoring.validator',
