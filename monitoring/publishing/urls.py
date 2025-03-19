a1821cf02bfd1263aff57263d28d8e050ba26448
Sae126V <saitejav2021@gmail.com>
2025-03-19 16:01:08 +0000

    Update code to allow underscore in an regex pattern

--- monitoring/publishing/urls.py
+++ monitoring/publishing/urls.py
@@ -10,22 +10,22 @@ router.register(r'gridsync', views.GridSiteSyncViewSet)
 
 urlpatterns = [
     re_path(
-        r'^cloud/(?P<SiteName>[a-zA-Z0-9.-]+)/$',
+        r'^cloud/(?P<SiteName>[a-zA-Z0-9._-]+)/$',
         views.CloudSiteViewSet.as_view({'get': 'retrieve'}),
         name='cloudsite-detail'
     ),
     re_path(
-        r'^grid/(?P<SiteName>[a-zA-Z0-9.-]+)/$',
+        r'^grid/(?P<SiteName>[a-zA-Z0-9._-]+)/$',
         views.GridSiteViewSet.as_view({'get': 'retrieve'}),
         name='gridsite-detail'
     ),
     re_path(
-        r'^gridsync/(?P<SiteName>[a-zA-Z0-9.-]+)/$',
+        r'^gridsync/(?P<SiteName>[a-zA-Z0-9._-]+)/$',
         views.GridSiteSyncViewSet.as_view({'get': 'retrieve'}),
         name='gridsitesync-detail'
     ),
     re_path(
-        r'^gridsync/(?P<SiteName>[a-zA-Z0-9.-]+)/(?P<YearMonth>[0-9-]+)/$',
+        r'^gridsync/(?P<SiteName>[a-zA-Z0-9._-]+)/(?P<YearMonth>[0-9-]+)/$',
         views.GridSiteSyncSubmitHViewSet.as_view({'get': 'retrieve'}),
         name='gridsync-submithost'
     ),
