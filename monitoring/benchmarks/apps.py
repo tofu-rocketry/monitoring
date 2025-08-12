5e36e49d16c0454b0301e27ff2a7a379aebba51b
Manoj Garai <manoj.garai@stfc.ac.uk>
2025-08-12 11:12:19 +0100

    Update app name

--- monitoring/benchmarks/apps.py
+++ monitoring/benchmarks/apps.py
@@ -2,5 +2,4 @@ from django.apps import AppConfig
 
 
 class BenchmarksConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'benchmarks'
+    name = 'monitoring.benchmarks'
