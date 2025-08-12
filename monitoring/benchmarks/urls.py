07d9191b59d7328a12b8cfea68c607678486c4b1
Manoj Garai <manoj.garai@stfc.ac.uk>
2025-08-12 13:19:11 +0100

    Update url

--- monitoring/benchmarks/urls.py
+++ monitoring/benchmarks/urls.py
@@ -4,11 +4,11 @@ from monitoring.benchmarks import views
 from django.urls import re_path
 
 router = routers.SimpleRouter()
-router.register(r'benchmarks', views.BenchmarksViewSet)
+router.register('', views.BenchmarksViewSet)
 
 urlpatterns = [
     re_path(
-        r'^benchmarks/(?P<SiteName>[a-zA-Z0-9._-]+)/$',
+        r'/(?P<SiteName>[a-zA-Z0-9._-]+)/$',
         views.BenchmarksViewSet.as_view({'get': 'retrieve'}),
         name='benchmarksbysubmithost-details'
     ),
