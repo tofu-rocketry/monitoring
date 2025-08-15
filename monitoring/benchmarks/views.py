1f380d50ee2269fa5e6da822b7ecf4afa1b3dd4b
Manoj Garai <manoj.garai@stfc.ac.uk>
2025-08-15 12:02:38 +0100

    Fetch data from two tables

--- monitoring/benchmarks/views.py
+++ monitoring/benchmarks/views.py
@@ -15,22 +15,6 @@ from monitoring.benchmarks.models import BenchmarksBySubmithost
 
 from monitoring.benchmarks.serializers import BenchmarksBySubmithostSerializer
 
-def update_dict_stdout_and_returncode(single_dict, date):
-    diff = datetime.today() - date
-    date = date.strftime("%Y-%m-%d")
-
-    if diff <= timedelta(days=7):
-        single_dict['returncode'] = 0
-        single_dict['stdout'] = "OK [ last published %s days ago: %s ]" % (diff.days, date)
-    elif diff > timedelta(days=7):
-        single_dict['returncode'] = 1
-        single_dict['stdout'] = "WARNING [ last published %s days ago: %s ]" % (diff.days, date)
-    else:
-        single_dict['returncode'] = 3
-        single_dict['stdout'] = "UNKNOWN"
-    return single_dict
-
-
 class BenchmarksViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = BenchmarksBySubmithost.objects.all()
     serializer_class = BenchmarksBySubmithostSerializer
@@ -47,8 +31,7 @@ class BenchmarksViewSet(viewsets.ReadOnlyModelViewSet):
 
         for single_dict in response.data:
             date = single_dict.get('UpdateTime').replace(tzinfo=None)
-            single_dict = update_dict_stdout_and_returncode(single_dict, date)
-            final_response.append(single_dict)
+            final_response.append(date)
 
         if type(request.accepted_renderer) is TemplateHTMLRenderer:
             response.data = {
@@ -63,17 +46,21 @@ class BenchmarksViewSet(viewsets.ReadOnlyModelViewSet):
         # If there's no data then last_fetched is None.
         if last_fetched is not None:
             print(last_fetched.replace(tzinfo=None), datetime.today() - timedelta(hours=1, seconds=20))
-
-        response = super(BenchmarksViewSet, self).retrieve(request)
-        date = response.data['UpdateTime'].replace(tzinfo=None)
-        response.data = update_dict_stdout_and_returncode(response.data, date)
+        
+        final_response = []
+        sites_list_qs = BenchmarksBySubmithost.objects.filter(SiteName=SiteName)
+        sites_list_serializer = self.get_serializer(sites_list_qs, many=True)
+        
+        for single_dict in sites_list_serializer.data:
+            date = single_dict.get('UpdateTime').replace(tzinfo=None)
+            final_response.append(date)
 
         # Wrap data in a dict so that it can display in template.
         if type(request.accepted_renderer) is TemplateHTMLRenderer:
             # Single result put in list to work with same HTML template.
-            response.data = {
-                'benchmark': [response.data],
+            response = {
+                'benchmarks': final_response,
                 'last_fetched': last_fetched
             }
 
-        return response
+        return Response(response)
