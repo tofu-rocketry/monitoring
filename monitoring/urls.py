48b8a4b050d6deb8d50cbbefef03abf81150ad46
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2025-08-21 15:36:18 +0100

    Update site base urls to use newer path() function

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -1,26 +1,10 @@
-"""Monitoring URL Configuration.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import include, url
+from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^availability/', include('monitoring.availability.urls')),
-    url(r'^publishing/', include('monitoring.publishing.urls')),
-    url(r'^benchmarks/', include('monitoring.benchmarks.urls')),
-    url(r'^api-auth/', include('rest_framework.urls',
-                               namespace='rest_framework')),
+    path('admin/', admin.site.urls),
+    path('availability/', include('monitoring.availability.urls')),
+    path('publishing/', include('monitoring.publishing.urls')),
+    path('benchmarks/', include('monitoring.benchmarks.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
