27a0437aeffd02a20a97c5e79a53b7856fb447e5
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2025-08-21 17:57:56 +0100

    Add names to project urls for later referencing

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -4,9 +4,9 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='home.html')),
-    path('availability/', include('monitoring.availability.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('availability/', include('monitoring.availability.urls'), name='availability'),
     path('publishing/', include('monitoring.publishing.urls')),
-    path('benchmarks/', include('monitoring.benchmarks.urls')),
+    path('benchmarks/', include('monitoring.benchmarks.urls'), name='benchmarks'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
