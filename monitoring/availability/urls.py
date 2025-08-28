8c97355ee375365040988ca5278313a2c2657f7f
Adrian Coveney <4836233+tofu-rocketry@users.noreply.github.com>
2025-08-28 11:15:46 +0100

    Convert remaining url() to path() (#58)
    url() is deprecating and is directly replaced by re_path(), but the
    new simpler path() function suits this use case.

--- monitoring/availability/urls.py
+++ monitoring/availability/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.urls import path
 
 from monitoring.availability import views
 
 urlpatterns = [
-    url(r'^$', views.status, name='availability'),
+    path('', views.status, name='availability'),
 ]
