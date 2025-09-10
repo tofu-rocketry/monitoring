d311d73c0e3d4fbc188203e2dc9af8084a65501a
Manoj Garai <manoj.garai@stfc.ac.uk>
2025-09-10 14:54:29 +0100

    Add unique key for BenchmarksBySubmithost

--- monitoring/benchmarks/models.py
+++ monitoring/benchmarks/models.py
@@ -12,7 +12,7 @@ class BenchmarksBySubmithost(models.Model):
 
     class Meta:
         ordering = ('SiteName',)
-
+        unique_together = ('SiteName', 'SubmitHost', 'RecordType')
 
 class VJobRecords(models.Model):
     Site = models.CharField(max_length=255, primary_key=True)
