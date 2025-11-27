c733aa07650859f62d52e5f937005808b9f3df0a
Rowan Moss <rowan.moss@stfc.ac.uk>
2025-11-27 16:28:12 +0000

    Update validator app config
    - within apps.py
    - update config to have the correct name

--- monitoring/validator/apps.py
+++ monitoring/validator/apps.py
@@ -2,5 +2,4 @@ from django.apps import AppConfig
 
 
 class ValidatorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'validator'
+    name = 'monitoring.validator'
