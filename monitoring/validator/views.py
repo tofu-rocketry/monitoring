f95789b3358a461b48a1ef3eb3e9edf731094ddc
Rowan Moss <rowan.moss@stfc.ac.uk>
2025-12-09 17:24:57 +0000

    Create initial html page template and validator view
    - Linked into the Apel RecordFactory
    - Doesn't make use of django model

--- monitoring/validator/views.py
+++ monitoring/validator/views.py
@@ -1,5 +1,51 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
 
+from apel.db.loader.record_factory import RecordFactory
+
+
+@require_http_methods(["GET", "POST"])
 def index(request):
-    return HttpResponse("Hello. This is validator index")
+    """
+    Validates inputted records using Apel RecordFactory,
+    when html form is submitted.
+    """
+    template_name = "validator/validator_index.html"
+    input_record = ""
+    output = ""
+
+
+    def validate(record: str) -> str:
+        """
+        Validated record(s) passed in, and returns the result
+        """
+        if not record:
+            return "Please enter a record to be validated."
+
+        record = record.strip()
+
+        try:
+            recordFactory = RecordFactory()
+
+            result = recordFactory.create_records(record)
+
+            return str(result)
+
+        except Exception as e:
+            return str(e)
+
+
+    # On form submission, trigger record validation
+    if request.method == "POST":
+        input_record = request.POST.get("input_record", "")
+        try:
+            output = validate(input_record)
+        except Exception as e:
+            output = f"Error during validation: {e}"
+
+    context = {
+        "input_record": input_record,
+        "output": output,
+    }
+
+    return render(request, template_name, context)
