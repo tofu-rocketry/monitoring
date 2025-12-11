9954c7f6ba0eeb6e9b9c640e6471f5408b9903af
Rowan Moss <rowan.moss@stfc.ac.uk>
2025-12-11 17:43:54 +0000

    Add dropdown selection for record type
    - has to import the apel record type classes
    - has an option for all, which uses the record header instead

--- monitoring/validator/views.py
+++ monitoring/validator/views.py
@@ -3,6 +3,14 @@ from django.views.decorators.http import require_http_methods
 
 from apel.db.loader.record_factory import RecordFactory
 
+# Apel record class imports
+from apel.db.records.job import JobRecord, JobRecord04
+from apel.db.records.summary import SummaryRecord, SummaryRecord04
+from apel.db.records.normalised_summary import NormalisedSummaryRecord, NormalisedSummaryRecord04
+from apel.db.records.sync import SyncRecord
+from apel.db.records.cloud import CloudRecord
+from apel.db.records.cloud_summary import CloudSummaryRecord
+
 
 @require_http_methods(["GET", "POST"])
 def index(request):
@@ -12,10 +20,24 @@ def index(request):
     """
     template_name = "validator/validator_index.html"
     input_record = ""
+    record_type = "All"
     output = ""
 
+    # Map record_type string to record_type class
+    record_map = {
+        "JobRecord": JobRecord,
+        "JobRecord04": JobRecord04,
+        "SummaryRecord": SummaryRecord,
+        "SummaryRecord04": SummaryRecord04,
+        "NormalisedSummaryRecord": NormalisedSummaryRecord,
+        "NormalisedSummaryRecord04": NormalisedSummaryRecord04,
+        "SyncRecord": SyncRecord,
+        "CloudRecord": CloudRecord,
+        "CloudSummaryRecord": CloudSummaryRecord,
+    }
 
-    def validate(record: str) -> str:
+
+    def validate(record: str, record_type: str) -> str:
         """
         Validated record(s) passed in, and returns the result
         """
@@ -27,7 +49,11 @@ def index(request):
         try:
             recordFactory = RecordFactory()
 
-            result = recordFactory.create_records(record)
+            if record_type == "All":
+                result = recordFactory.create_records(record)
+            else:
+                record_class = record_map[record_type]
+                result = recordFactory._create_record_objects(record, record_class)
 
             if "Record object at" in str(result):
                 return "Record(s) valid!"
@@ -41,10 +67,12 @@ def index(request):
     # On form submission, trigger record validation
     if request.method == "POST":
         input_record = request.POST.get("input_record", "")
-        output = validate(input_record)
+        record_type = request.POST.get("record_type", "")
+        output = validate(input_record, record_type)
 
     context = {
         "input_record": input_record,
+        "record_type": record_type,
         "output": output,
     }
 
