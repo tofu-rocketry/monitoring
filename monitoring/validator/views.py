5f9a88a1a72034131d96b2b4734eb5bc3d65276b
Rowan Moss <rowan.moss@stfc.ac.uk>
2025-12-18 15:11:44 +0000

    Add improvement changes, including refactoring and improved styling
    - adds more comments
    - reworks exception handling
    - adds apel packages to requirements
    - adds styling
    - adds link to validator page onto home monitoring page

--- monitoring/validator/views.py
+++ monitoring/validator/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 
-from apel.db.loader.record_factory import RecordFactory
+# Apel record-checking class imports
+from apel.db.loader.record_factory import RecordFactory, RecordFactoryException
+from apel.db.records.record import InvalidRecordException
 
-# Apel record class imports
+# Apel record-type class imports
 from apel.db.records.job import JobRecord, JobRecord04
 from apel.db.records.summary import SummaryRecord, SummaryRecord04
 from apel.db.records.normalised_summary import NormalisedSummaryRecord, NormalisedSummaryRecord04
@@ -15,8 +17,11 @@ from apel.db.records.cloud_summary import CloudSummaryRecord
 @require_http_methods(["GET", "POST"])
 def index(request):
     """
-    Validates inputted records using Apel RecordFactory,
-    when html form is submitted.
+    Validates inputted records using the Apel record validation methods.
+    It either validates a record against a specific type or against all types, depending on what record_type
+        option was chosen on the html template. The default is `All`.
+    The input record, record type and validation output are then returned to the html template as context on get
+        request, so that the html page retains its information/context when refreshing the page or submitting the form.
     """
     template_name = "validator/validator_index.html"
     input_record = ""
@@ -24,6 +29,7 @@ def index(request):
     output = ""
 
     # Map record_type string to record_type class
+    # String is always exact as determined through html form selection option
     record_map = {
         "JobRecord": JobRecord,
         "JobRecord04": JobRecord04,
@@ -39,7 +45,12 @@ def index(request):
 
     def validate(record: str, record_type: str) -> str:
         """
-        Validated record(s) passed in, and returns the result
+        Validated record(s) and record_type passed in from the html page template.
+        If record type is all, make use of the create_records apel method (expects a record header).
+        Else, make use of the _create_record_objects apel method (expects there to be no record header).
+        If the record is valid, return a "valid record" string.
+        If the record is invalid, an InvalidRecordException or RecordFactoryException is raised by the Apel methods.
+        Catch these exceptions and return the exception information.
         """
         if not record:
             return "Please enter a record to be validated."
@@ -57,10 +68,10 @@ def index(request):
 
             if "Record object at" in str(result):
                 return "Record(s) valid!"
-            else:
-                return str(result)
 
-        except Exception as e:
+            return str(result)
+
+        except (InvalidRecordException, RecordFactoryException) as e:
             return str(e)
 
 
