006df8c32a7db0aa23b7a5d6950c737e852b355f
Rowan Moss <rowan.moss@stfc.ac.uk>
2026-01-14 16:49:14 +0000

    Add status column to iris view
    - like in the grid view
    - uses publishing update_dict_stdout_and_returncode method

--- monitoring/iris/views.py
+++ monitoring/iris/views.py
@@ -10,6 +10,8 @@ from rest_framework.renderers import TemplateHTMLRenderer
 from monitoring.iris.models import IrisCloudAndGrid
 from monitoring.iris.serializers import IrisCloudAndGridSerializer
 
+from monitoring.publishing.views import update_dict_stdout_and_returncode
+
 
 class IrisViewSet(viewsets.ReadOnlyModelViewSet):
     # Lower('SiteName'): sorts sites alphabetically, case-insensitively.
@@ -23,11 +25,17 @@ class IrisViewSet(viewsets.ReadOnlyModelViewSet):
     def list(self, request):
         last_fetched = IrisCloudAndGrid.objects.aggregate(Max('fetched'))['fetched__max']
 
+        final_response = []
         response = super(IrisViewSet, self).list(request)
+
+        for single_dict in response.data:
+            date = single_dict.get('UpdateTime').replace(tzinfo=None)
+            single_dict = update_dict_stdout_and_returncode(single_dict, date)
+            final_response.append(single_dict)
 
         if type(request.accepted_renderer) is TemplateHTMLRenderer:
             response.data = {
-                'irisCloudAndGridData': response.data,
+                'irisCloudAndGridData': final_response,
                 'last_fetched': last_fetched,
             }
 
